ee3b24ef750c7e803f67b1283bed7aa6a6a50319
Josiah Kerley <josiahkerley@gmail.com>
2017-06-27 17:47:17 -0600

    added ping

---
+++ pistonithformodem/pinger.py
@@ -0,0 +1,25 @@
+import os
+
+class Pinger(object):
+  '''
+  Handles monitoring by pinging a given host
+  '''
+
+  def __init__(self,host,count=1):
+    '''
+    Constructor
+    :param host: hostname or ip of monitored host
+    '''
+    self.host = host   ## Add host param to object
+    self.count = count ## Number of pings to try before failing
+
+  def ping(self):
+    '''
+    Performs a ping
+    :return: If the ping was succesful, returns true, else false
+    '''
+    cmd = "ping -c {} {}".format(str(self.count),self.host) ## Literal string of the ping command to exec
+    if os.system(cmd) == 0: ## Runs the command and if the result is 0, then the ping was good
+      return(True)
+    else:
+      return(False)
