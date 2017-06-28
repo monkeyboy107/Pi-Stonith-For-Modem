bb594b37e0ca5f9c94a6bf7b4846698df87319a8
Josiah Kerley <josiahkerley@gmail.com>
2017-06-27 19:47:15 -0600

    added resolver

--- pistonithformodem/pinger.py
+++ pistonithformodem/pinger.py
@@ -16,10 +16,11 @@ class Pinger(object):
   def ping(self):
     '''
     Performs a ping
-    :return: If the ping was succesful, returns true, else false
+    :return: If the ping was successful, returns true, else false
     '''
     cmd = "ping -c {} {}".format(str(self.count),self.host) ## Literal string of the ping command to exec
     if os.system(cmd) == 0: ## Runs the command and if the result is 0, then the ping was good
       return(True)
     else:
       return(False)
+
